0e781fc62853f6d5afce58a9e0640215f3eb79e4
Lucas Henrique <lucashenriquecalixto355@gmail.com>
2026-04-01 13:27:35 -0300

    Finish base HTML/CSS from ecosystem section

--- mentaris-app/src/components/HomePage.tsx
+++ mentaris-app/src/components/HomePage.tsx
@@ -1,16 +1,23 @@
-import { Header } from './header/Header'
-import './HomePage.css'
+import { Header } from "./header/Header";
+import "./HomePage.css";
 import background from "../assets/images/background.png";
-
-
 
 export function HomePage() {
   return (
     <>
-    <Header />
-    <section id='hero'>
-      <img src={background} className='hero-image' alt="Background Image" />
-    </section>
+      <Header />
+      <section id="hero">
+        <img src={background} className="hero-image" alt="Background Image" />
+      </section>
+
+      <section id="ecosystem">
+        <h2 className="ecosystem-title">Tratamento descomplicado</h2>
+        <div className="systems-container">
+          <div className="system"></div>
+          <div className="system"></div>
+          <div className="system"></div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
