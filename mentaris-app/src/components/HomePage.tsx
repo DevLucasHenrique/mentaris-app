39cdd920b1037c87eba2dcfcbfbbcf07fbb3ca7b
Lucas Henrique <lucashenriquecalixto355@gmail.com>
2026-04-01 16:39:13 -0300

    Finish virtual store card from ecosystem section

--- mentaris-app/src/components/HomePage.tsx
+++ mentaris-app/src/components/HomePage.tsx
@@ -1,6 +1,9 @@
 import { Header } from "./header/Header";
 import "./HomePage.css";
 import background from "../assets/images/background.png";
+import arrow from "../assets/icons/arrow-up.svg"
+import bagsImage from "../assets/images/bags.png"
+import beach from "../assets/images/beach.png"
 
 export function HomePage() {
   return (
@@ -13,11 +16,22 @@ export function HomePage() {
       <section id="ecosystem">
         <h2 className="ecosystem-title">Tratamento descomplicado</h2>
         <div className="systems-container">
-          <div className="system"></div>
-          <div className="system"></div>
-          <div className="system"></div>
+          <div className="system">
+            <div className="info">
+              <h4 className="system-title">Loja Virtual</h4>
+              <p className="system-text">Expore nossa vritrine de produtos oficiais da Mentaris.</p>
+              <button className="store-button">Acessar Loja <img className="arrow-up" src={arrow} alt="" /></button>
+            </div>
+            <div className="images">
+              <img src={bagsImage} alt="Bags" />
+              <img src={beach} alt="Woman on Beach" />
+            </div>
+          </div>
+          
         </div>
       </section>
     </>
   );
 }
+
+
