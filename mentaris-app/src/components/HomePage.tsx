32ddd7d30030cd613ac418f3d961e78bcd788ebd
Lucas Henrique <lucashenriquecalixto355@gmail.com>
2026-04-01 13:01:00 -0300

    Finish Mobile Hero Section

--- mentaris-app/src/components/HomePage.tsx
+++ mentaris-app/src/components/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Header } from './header/Header'
 import './HomePage.css'
+import background from "../assets/images/background.png";
 
 
 
@@ -7,7 +8,9 @@ export function HomePage() {
   return (
     <>
     <Header />
-    
+    <section id='hero'>
+      <img src={background} className='hero-image' alt="Background Image" />
+    </section>
     </>
   )
 }
