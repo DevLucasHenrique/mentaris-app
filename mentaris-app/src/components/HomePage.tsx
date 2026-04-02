e28181aaf05ae08e10b3146fa1b959022ec10100
Lucas Henrique <lucashenriquecalixto355@gmail.com>
2026-04-02 18:34:14 -0300

    Add talk to a specialist button to the history section

--- mentaris-app/src/components/HomePage.tsx
+++ mentaris-app/src/components/HomePage.tsx
@@ -69,6 +69,7 @@ export function HomePage() {
         <p className="history-text">
           Líder em bem-estar e saúde mental no Brasil, a <img src={logoLemon} className="text-logo-image" alt="Logo" /> Mentaris transforma vidas com atendimento personalizado, tecnologia de ponta e profissionais apaixonados pelo que fazem. Você merece o melhor.
         </p>
+        <button className="talk">Falar com um especialista</button>
       </section>
     </>
   );
