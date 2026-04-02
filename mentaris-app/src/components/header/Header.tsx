65dfa7fff72eedbc5540b62c0e8af25f9cf95f96
Lucas Henrique <lucashenriquecalixto355@gmail.com>
2026-04-02 17:56:20 -0300

    Finish base HTML of mobile menu

--- mentaris-app/src/components/header/Header.tsx
+++ mentaris-app/src/components/header/Header.tsx
@@ -1,13 +1,24 @@
 import "./Header.css";
 import logo from "../../assets/images/logo.png";
+import { useState } from "react";
 export function Header() {
+  const [menuState, setMenuState] = useState("closed");
+
   return (
-    <header>
-      <img src={logo} className="logo-image" alt="Logo" />
-      <button className="menu-button">
-        <span className="bar1 bar"></span>
-        <span className="bar2 bar"></span>
-      </button>
-    </header>
+    <>
+      <header>
+        <img src={logo} className="logo-image" alt="Logo" />
+        <button
+          className={`menu-button ${menuState === "open"? 'menu-active': 'menu-closing'}`}
+          onClick={() => {
+            setMenuState(menuState === "closed" ? "open" : "closed");
+          }}
+        >
+          <span className="bar1 bar"></span>
+          <span className="bar2 bar"></span>
+        </button>
+      </header>
+      <div className={`mobile-menu ${menuState} `}></div>
+    </>
   );
 }
